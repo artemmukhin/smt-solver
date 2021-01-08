52bcff9318432549cd7f9d7cc4c670a67282b3e0
ortem <ortem00@gmail.com>
2021-01-08 15:13:55 +0300

    Add Term and Relation

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,60 @@
 #[cfg(test)]
 mod test;
+
+#[derive(Debug)]
+pub struct Term {
+    pub name: String,
+    pub arguments: Vec<Term>,
+}
+
+impl Term {
+    // TODO: Should parse "f(g(a, b))" to `Symbol { "f", [ Symbol { "g", ["a", "b"] } ] }`
+    #[allow(dead_code)]
+    fn from(_raw: String) -> Term { unimplemented!() }
+    
+    pub fn var(name: &str) -> Term {
+        Term { name: name.to_string(), arguments: vec![] }
+    }
+}
+
+#[derive(Debug)]
+pub enum RelationKind { Equal, NotEqual }
+
+/// Equality relation
+#[derive(Debug)]
+pub struct Relation {
+    pub left: Term,
+    pub right: Term,
+    pub kind: RelationKind,
+}
+
+impl Relation {
+    #[allow(dead_code)]
+    pub fn parse(raw: String) -> Relation {
+        let mut kind: RelationKind = RelationKind::Equal;
+        let mut eq_index = 0;
+
+        for char in raw.chars().enumerate() {
+            match char {
+                (i, '=') => {
+                    kind = RelationKind::Equal;
+                    eq_index = i
+                }
+                (i, '!') if raw.chars().nth(i + 1).expect("Invalid symbol") == '=' => {
+                    kind = RelationKind::NotEqual;
+                    eq_index = i;
+                }
+                _ => {}
+            }
+        }
+
+        let left_raw = &raw[eq_index..];
+        let right_raw = &raw[..eq_index];
+
+        Relation {
+            left: Term::from(left_raw.to_string()),
+            right: Term::from(right_raw.to_string()),
+            kind,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use smt_solver;
+use smt_solver::{Relation, Term, RelationKind};
+
 fn main() {
-    println!("Hello, world!");
+    // `f(x, y) = g(x)`
+    let relation = Relation {
+        left: Term { 
+            name: "f".to_string(),
+            arguments: vec![Term::var("x"), Term::var("y")]
+        },
+        right: Term {
+            name: "g".to_string(),
+            arguments: vec![Term::var("x")]
+        },
+        kind: RelationKind::Equal
+    };
+    
+    println!("{:#?}", relation);
 }
