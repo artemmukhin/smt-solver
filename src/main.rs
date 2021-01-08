b73b541f35d01e8d7e03ca2503e5de287f88dc90
ortem <ortem00@gmail.com>
2021-01-08 20:00:08 +0300

    Introduce Union-Find

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use smt_solver;
 use smt_solver::{Relation, Term, Kind};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
+use disjoint_sets::UnionFind;
 
 fn get_unsat_example() -> Vec<Relation> {
     vec![
         // `f(x, y) = x`
         Relation {
             left: Term::fun("f", vec![Term::var("x"), Term::var("y")]),
-            right: Term::fun("g", vec![Term::var("x")]),
+            right: Term::var("x"),
             kind: Kind::Equal,
         },
         // `f(f(x, y), y) != x`
@@ -22,19 +23,47 @@ fn get_unsat_example() -> Vec<Relation> {
     ]
 }
 
-fn print_relation(relation: Relation) {
-    println!("Relation: {}", relation);
-    let subterms = relation.subterms().collect::<HashSet<_>>();
+fn main() {
+    let relations = get_unsat_example();
+    let mut all_subterms: HashMap<Term, usize> = HashMap::new();
+    let mut index = 0;
+
+    for relation in relations.iter() {
+        println!("Relation: {}", relation);
+        let subterms = relation.subterms().collect::<HashSet<_>>();
+        for subterm in subterms {
+            if !all_subterms.contains_key(subterm) {
+                all_subterms.insert(subterm.clone(), index);
+                index += 1;
+            }
+        }
+    }
+    println!();
+
     println!("Subterms:");
-    for subterm in subterms {
+    for subterm in all_subterms.keys() {
         println!("{}", subterm);
     }
     println!();
-}
 
-fn main() {
-    let relations = get_unsat_example();
-    for relation in relations {
-        print_relation(relation);
+    let mut union_find = UnionFind::new(all_subterms.len());
+    let eq_relations = relations
+        .iter()
+        .filter(|relation| match relation.kind {
+            Kind::Equal => true,
+            Kind::NotEqual => false
+        })
+        .collect::<Vec<_>>();
+
+    for relation in eq_relations {
+        let s = *all_subterms.get(&relation.left).unwrap();
+        let t = *all_subterms.get(&relation.right).unwrap();
+        union_find.union(s, t);
+        // TODO: propagate the new congruence with symmetry, transitivity, and functional congruence
+    }
+
+    println!("Union-Find");
+    for (subterm, i) in all_subterms.iter() {
+        println!("{} -> {}", subterm, union_find.find(*i));
     }
 }
